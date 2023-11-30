68bfc9f99709e9da97d34faafa2875bca346cec1
Sarah <120528945+sarahGruber6@users.noreply.github.com>
2023-11-30 15:02:08 -0600

    Add files via upload

---
+++ debug/sounds/images/other/aesprite files/subdir_vars.mk
@@ -0,0 +1,14 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+EXE_SRCS += \
+../images/other/aesprite\ files/BmpConvert16.exe 
+
+EXE_SRCS__QUOTED += \
+"../images/other/aesprite files/BmpConvert16.exe" 
+
+
